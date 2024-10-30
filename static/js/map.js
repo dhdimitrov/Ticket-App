b6843a57e6a83d08a26bc1bb5c159b9362bbf735
Daniel Dimitrov <daniel.h.dimitrov@itgix.com>
2024-10-30 11:37:20 +0200

    Initial commit: Add Bulgaria regions map

---
+++ static/js/map.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const width = 800;
+    const height = 600;
+
+    const svg = d3.select('#map')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+    const projection = d3.geoMercator()
+        .center([25.5, 42.7])
+        .scale(3500)
+        .translate([width / 2, height / 2]);
+
+    const path = d3.geoPath().projection(projection);
+
+    d3.json('/static/data/bulgaria-regions.json').then(function(data) {
+        svg.selectAll('.region')
+            .data(data.features)
+            .enter()
+            .append('path')
+            .attr('class', 'region')
+            .attr('d', path)
+            .on('click', function(event, d) {
+                alert('You clicked on ' + d.properties.name);
+            });
+    });
+});
