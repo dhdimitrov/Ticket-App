14af4684a232c6f30b3131fbcaba9b6cab7dea31
Daniel Dimitrov <daniel.h.dimitrov@itgix.com>
2024-10-30 15:36:10 +0200

    Functionalities and beautify

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, jsonify
 import requests
 from bs4 import BeautifulSoup
 from deep_translator import GoogleTranslator
+import re
 
 app = Flask(__name__)
 
@@ -29,14 +30,17 @@ def biletbg():
                 
     concerts = []    
     for event in soup.find_all('div', class_='upcoming-events-box'):
+        image_div = event.find('div', class_='img-top')
+        image = extract_image_url(image_div)
         name = event.find('h5').text.strip()
-            # date = event.find('span', class_='date').text.strip()
-        concerts.append({"name": name})
+        place = event.find(class_='upcoming-events-place').text.strip()
+        date = event.find('span').text.strip()
+        concerts.append({"image": image, "name": name, "place": place, "date": date})
     return concerts
 
 def grabo(region):
-    city = translate_city(region)
-    url = f"https://grabo.bg/events/{city.lower()}/popular#s"
+    city = translate_city(region).lower().replace(" ", "-")
+    url = f"https://grabo.bg/events/{city}/popular#s"
     response = requests.get(url)
     concerts = []
     if response.status_code == 200:
@@ -45,17 +49,31 @@ def grabo(region):
         events = soup.find_all('div', class_='e_deal')
 
         for event in events:
+            image_div = event.find('div', class_='ed_image')
             title_div = event.find('div', class_='ed_event_title')
+            place_div = event.find('div', class_='ed_venue')
+            date_div  = event.find('div', class_='ed_date')
             
             if title_div:  
+                image = extract_image_url(image_div)
                 title = title_div.text.strip()
-                concerts.append({"name": title})
+                place = place_div.text.strip() if place_div else ""
+                date = date_div.text.strip() if date_div else ""
+                concerts.append({"image": image, "name": title, "place" : place, "date" : date})
     return concerts
 
 def translate_city(city_name):
     # Translate the city name from Bulgarian to English
     translated = GoogleTranslator(source='bg', target='en').translate(city_name)
     return translated
+
+def extract_image_url(image_div):
+    if image_div and 'style' in image_div.attrs:
+        style = image_div['style']
+        url_match = re.search(r'url\([\'"]?(.+?)[\'"]?\)', style)
+        if url_match:
+            return url_match.group(1)
+    return ""
 
 if __name__ == '__main__':
     app.run(debug=True)
